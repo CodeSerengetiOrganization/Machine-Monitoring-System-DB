cd4598008203947ef01535efc97594df1017f305
CodeSerengetiOrg <codeserengetiorg@gmail.com>
2025-09-18 16:21:28 -0400

    add code to add column for product sequence, add unsaved code

--- delta/V2_add_column_f_product_seq.sql
+++ delta/V2_add_column_f_product_seq.sql
@@ -3,9 +3,32 @@ USE kir_server_db
 -- step1: inser the column
 ALTER TABLE t_manufacturing_simple_result 
 ADD COLUMN f_product_seq BIGINT UNSIGNED NULL AFTER f_product_code;
--- step2: polulate the column
 
--- step3:set column to `NOT NULL`
+-- step2: polulate the column
+  -- initialize a variable
+SET @seq = 0;
+
+  -- update the table in the order of f_end_time
+UPDATE t_manufacturing_simple_result
+SET f_product_seq = (@seq := @seq + 1)
+ORDER BY f_end_time ASC;
+
+  -- step3:set column to `NOT NULL`
+ALTER TABLE t_manufacturing_simple_result
+MODIFY COLUMN f_product_seq BIGINT UNSIGNED NOT NULL;
+
+
+
+-- tool scrips for checking
+DESCRIBE t_manufacturing_simple_result
+
+SELECT * FROM t_manufacturing_simple_result 
+	ORDER BY f_start_time ASC 
+	LIMIT 10;
+
+SELECT * FROM t_manufacturing_simple_result 
+	ORDER BY f_start_time DESC;
+
 
 
 -- another solution
